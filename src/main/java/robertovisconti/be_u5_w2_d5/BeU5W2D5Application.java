f407a7ff2a5a6f5ad61f6f7c4d4c7ee3b38d567f
Roberto Visconti <robertovisconti93@gmail.com>
2026-07-10 09:34:27 +0200

    Initial commit

---
+++ src/main/java/robertovisconti/be_u5_w2_d5/BeU5W2D5Application.java
@@ -0,0 +1,13 @@
+package robertovisconti.be_u5_w2_d5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeU5W2D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeU5W2D5Application.class, args);
+	}
+
+}
---
+++ src/test/java/robertovisconti/be_u5_w2_d5/BeU5W2D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package robertovisconti.be_u5_w2_d5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU5W2D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
